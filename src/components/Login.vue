efa0c83517f01520eaf8a842b41af82655f2cb98
bentiancai629 <bentiancai629@gmail.com>
2020-03-08 21:20:46 +0800

    20 表单重置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,65 @@
 <template>
     <div class="login_container">
         <div class="login_box">
+            <!-- 头像区 -->
             <div class="avatoar_box">
                 <img src="../assets/logo.png" alt="">
             </div>
+            <!-- 登录表单区域 -->
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" placeholder="用户名"
+                                prefix-icon="iconfont icon-user">
+                    </el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                 <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" placeholder="密码"
+                                prefix-icon="iconfont icon-3702mima"
+                                type="password" >
+                    </el-input>
+                </el-form-item>
+                <!-- 按钮 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+    return {
+        loginForm: {
+            username: 'zs',
+            passwword: '123'
+        },
+        // 这是表单验证规则对象
+        loginFormRules: {
+            //验证用户名是否合法
+            username: [
+              { required: true, message: '请输入用户名称', trigger: 'blur' },
+              { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+            ],
+            password: [
+                { required: true, message: '请输入登陆密码', trigger: 'blur' },
+              { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+            ]
+        }
+      }
+    },
+    methods: {
+        // 重置登陆按钮
+        resetLoginForm () {
+            // console.log(this)
+            this.$refs.loginFormRef.resetFields()
+        }
+    }
+}
 </script>
 
 <style lang="less" scoped>
@@ -46,5 +96,18 @@ export default {}
             background-color: #eee;
         }
     }
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+
+.btns {
+    display: flex;
+    justify-content: flex-end;  //居右对齐
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-//导入全局样式表
+// 导入全局样式表
 import './assets/css/global.css'
+// 导入图标库
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
