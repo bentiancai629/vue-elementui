5812690509846703f1196951ad1af993b02a4dbe
bentiancai629 <bentiancai629@gmail.com>
2020-03-09 22:52:36 +0800

    29 登陆界面

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入图标库
 import './assets/fonts/iconfont.css'
+// axios
+import axios from 'axios'
+// 配置请求的路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+
+// 挂载到vue的原属性
+Vue.prototype.$message = Message
